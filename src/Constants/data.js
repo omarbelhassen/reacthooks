fb779a24e2873d02e813a9a9a4ed5e2068e8b5fb
omargit config --global user.name omargit config --global user.name omar <omarbelhassen16@gmail.com>
2020-11-25 16:12:31 +0100

    first commit

---
+++ src/Constants/data.js
@@ -0,0 +1,79 @@
+export const moviesData = [
+    {
+      id: Math.random(),
+      name: "The Godfather",
+      rating: 4,
+      description:
+        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+      date: 1972,
+      type: "Crime, Drama",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY209_CR3,0,140,209_AL_.jpg",
+    },
+    {
+      id: Math.random(),
+      name: "Predestination",
+      rating: 4,
+      description:
+        "For his final assignment, a top temporal agent must pursue the one criminal that has eluded him throughout time. The chase turns into a unique, surprising and mind-bending exploration of love, fate, identity and time travel taboos.",
+      date: 2014,
+      type: "Action, Drama, Sci-Fi",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMTAzODc3NjU1NzNeQTJeQWpwZ15BbWU4MDk5NTQ4NTMx._V1_UY209_CR1,0,140,209_AL_.jpg",
+    },
+    {
+      id: Math.random(),
+      name: "Interstellar",
+      rating: 4,
+      description:
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      date: 2014,
+      type: "Adventure, Drama, Sci-Fi",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    },
+    {
+      id: Math.random(),
+      name: " The Dictator",
+      rating: 3,
+      description:
+        "The heroic story of a dictator who risked his life to ensure that democracy would never come to the country he so lovingly oppressed.",
+      date: 2012,
+      type: "Comedy",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BNTlkOWYzZDYtNzQ1MS00YTNkLTkyYTItMjBmNjgyMDBlMjI4XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    },
+    {
+      id: Math.random(),
+      name: "Ali G Indahouse",
+      rating: 3,
+      description:
+        "Ali G unwittingly becomes a pawn in the Chancellor's plot to overthrow the Prime Minister of Great Britain. However, Ali is embraced by the nation as a voice of the youth, making the PM and his government more popular than ever.",
+      date: 2002,
+      type: "Comedy",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMTgxMTA5YmYtNTE0MC00Mzk1LWJkNTUtZjJiYzBjYjdlYTM4XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    },
+    {
+      id: Math.random(),
+      name: " Gladiator",
+      rating: 4,
+      description:
+        "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+      date: 2000,
+      type: "Action, Adventure, Drama",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY209_CR0,0,140,209_AL_.jpg",
+    },
+    {
+      id: Math.random(),
+      name: "300",
+      rating: 3,
+      description:
+        "King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 B.C.",
+      date: 2006,
+      type: "Action, Drama",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BNWMxYTZlOTUtZDExMi00YzZmLTkwYTMtZmM2MmRjZmQ3OGY4XkEyXkFqcGdeQXVyMTAwMzUyMzUy._V1_UY209_CR1,0,140,209_AL_.jpg",
+    },
+  ];
